d7189c6731aeb19ab0893896b9bdfafc56eac03b
Mike Stenzler <mike@nexuscafe.com>
2015-05-12 22:32:34 -0400

    Add delete remote event link

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,5 +1,21 @@
 $(document).on('click', 'form .remove_fields', function(event) {
-  $(this).prev('input[type=hidden]').val('1');
+  console.log("Cliked .remove_fields");
+//  var fs = $(this).closest('fieldset');
+//  console.log("fs macthed obj =");
+//  console.log(fs);
+//  var inp = fs.find('input[type=hidden]');
+//  console.log("fs input =");
+//  console.log(inp);
+
+ //$(this).prev('input[type=hidden]').val('1');
+ var inputelm = $(this).closest('fieldset').find('input[type=hidden]');
+ console.log("inputelm =");
+ console.log(inputelm)
+ inputelm.val('true');
+ console.log("inputelm.val = ");
+ console.log(inputelm.val());
+
+ // $(this).closest('fieldset')
   $(this).closest('fieldset').hide();
   return event.preventDefault();
 });
--- app/models/events/remote_event.rb
+++ app/models/events/remote_event.rb
@@ -24,7 +24,7 @@ class RemoteEvent < Event
   } 
 
   has_one :remote_event_api, :dependent => :destroy
-  accepts_nested_attributes_for :remote_event_api, update_only: true
+  accepts_nested_attributes_for :remote_event_api, allow_destroy: true, update_only: true
 #  has_many :remote_event_api_details
 
  # before_validation :populate_remote_event_api, on: :create
@@ -44,44 +44,47 @@ class RemoteEvent < Event
     ret
   end
 
-  def must_have_linked_events
-    errors.add(:base, 'Must have at least one linked event') if linked_events.all?(&:marked_for_destruction?)
-  end
+  private
 
-  def must_have_remote_api_key
-    unless (remote_event_api.api_key) 
-      errors.add(:base, 'Must have an api key')
+    def must_have_linked_events
+      errors.add(:base, 'Must have at least one linked event') if linked_events.all?(&:marked_for_destruction?)
     end
-  end
 
-  def populate_remote_event_api
-    logger.debug("****IN populate_remote_event_api. current remote_event_api=")
-    logger.debug(remote_event_api)
-    re_api = remote_event_api || RemoteEventApi.new
-#    re_api.remote_event_id = self.id
-#    if remember_api_key == 1
-#      re_api.api_key = remote_api_key
-#    end
-    event_urls = []
-    linked_events.map { |e| event_urls << e.url  }
+    def must_have_remote_api_key
+      has_key = remote_event_api.api_key.empty? ? false : true
+      unless (has_key) 
+        errors.add(:base, 'Must have an api key')
+      end
+    end
 
-    rclient = CCMeetup::Client.new({ auth_method: :api_key, api_key: remote_event_api.api_key })
-    re = CCRemoteEvent::ApiBuilder.new(rclient)
-    api = re.build(:meetup, { get_signed_url: true, url_list: event_urls, remember_api_key: remote_event_api.remember_api_key })
-    primary_event_index = api.primary_remote_event_index
-    primary_event = api.remote_event_api_details[primary_event_index]
-    logger.debug("primary_event =")
-    logger.debug(primary_event)
-    primary_start_date = api.remote_event_api_details[primary_event_index].start_date
-    #logger.debug("primary_start_date = " + primary_start_date)
-    if (primary_start_date)
-      logger.debug("!! Setting start date!!!")
-      self.start_date = primary_start_date
+    def populate_remote_event_api
+      logger.debug("****IN populate_remote_event_api. current remote_event_api=")
+      logger.debug(remote_event_api)
+      re_api = remote_event_api || RemoteEventApi.new
+  #    re_api.remote_event_id = self.id
+  #    if remember_api_key == 1
+  #      re_api.api_key = remote_api_key
+  #    end
+      event_urls = []
+      linked_events.map { |e| event_urls << e.url  }
+
+      rclient = CCMeetup::Client.new({ auth_method: :api_key, api_key: remote_event_api.api_key })
+      re = CCRemoteEvent::ApiBuilder.new(rclient)
+      api = re.build(:meetup, { get_signed_url: true, url_list: event_urls, remember_api_key: remote_event_api.remember_api_key })
+      primary_event_index = api.primary_remote_event_index
+      primary_event = api.remote_event_api_details[primary_event_index]
+      logger.debug("primary_event =")
+      logger.debug(primary_event)
+      primary_start_date = api.remote_event_api_details[primary_event_index].start_date
+      #logger.debug("primary_start_date = " + primary_start_date)
+      if (primary_start_date)
+        logger.debug("!! Setting start date!!!")
+        self.start_date = primary_start_date
+      end
+      if (primary_end_date = api.remote_event_api_details[primary_event_index].end_date)
+        self.end_date = primary_end_date
+      end
+      self.remote_event_api = api
     end
-    if (primary_end_date = api.remote_event_api_details[primary_event_index].end_date)
-      self.end_date = primary_end_date
-    end
-    self.remote_event_api = api
-  end
 
 end
