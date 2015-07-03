971804cc30efb119b5121bf926ae21a1a5532b8c
Mike Stenzler <mike@nexuscafe.com>
2015-07-02 23:52:00 -0400

    Remove require friendly_title_helper

--- app/models/events/local_event.rb
+++ app/models/events/local_event.rb
@@ -1,6 +1,7 @@
 class LocalEvent < Event
-  require friendly_title_helper
+ 
   include FriendlyTitleHelper
+  include SetModelNameHelper
 
   validates :description, presence: true
   validates :start_date, presence: true
