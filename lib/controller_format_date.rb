f298b99f36a490a5055fb1ab35db5f7b6f2260f0
Mike Stenzler <mike@nexuscafe.com>
2015-07-07 14:16:22 -0400

    Move Date function into FormatDate

--- lib/controller_format_date.rb
+++ lib/controller_format_date.rb
@@ -1,78 +1,10 @@
 module ControllerFormatDate
+
+  include FormatDate
+
   def self.included(controller)
-    controller.send :helper_method, :sdatetime, :s_date_time, :word_date_time, 
-                    :word_date, :american_date, :american_date_time, 
-                    :day_of_week, :month_name, :clock_time, :month_day_year,
-                    :short_word_date, :seconds_to_hours
-  end
-
-  def sdatetime(d)
-     ret = nil
-     begin
-       ret = d.strftime('%m-%d-%Y %I:%M:S %p')
-     rescue Exception => e
-       logger.error("ERROR in sdatetime! #{e})")
-     end
-     ret
-  end
-
-  def s_date_time(d)
-     convert_date(d, '%m-%d-%Y %l:%M %p')
-  end
-
-  def word_date_time(d)
-     convert_date(d, '%A, %B %d, %Y %l:%M %p')
-  end
-
-  def word_date(d)
-     convert_date(d, '%A, %B %d, %Y')
-  end
-
-  def short_word_date(d, options={})
-    wday = options[:full_weekday] ? '%A' : '%a'
-    month = options[:full_month] ? '%B' : '%b'
-    year = options[:year] ? ', %Y' : ''
-    convert_date(d, "#{wday}, #{month} %d#{year}")
-  end
-
-  def american_date(d)
-     convert_date(d, '%d/%m/%y')
-  end
-
-  def american_date_time(d)
-     convert_date(d, '%d/%m/%y %l:%M %p')
-  end
-
-  def day_of_week(d)
-     convert_date(d, '%A')
-  end
-
-  def month_name(d)
-     convert_date(d, '%B')
-  end
-
-  def clock_time(d)
-     convert_date(d, '%l:%M %p')
-  end
-
-  def month_day_year(d)
-    convert_date(d, '%m-%d-%y')
-  end
-
-  def seconds_to_hours(secs)
-    Time.at(secs).gmtime.strftime('%R:%S')
-  end
-
-  private
-
-  def convert_date(d, format)
-     ret = nil
-     begin
-       ret = d.strftime(format)
-     rescue Exception => e
-       logger.error("ERROR in comvert_date (date = #{d}. format = #{format})! #{e})")
-     end
-     ret
+    controller.send :helper_method, FormatDate::FUNCTIONS
   end
 
 end
+
--- lib/friendly_title_helper.rb
+++ lib/friendly_title_helper.rb
@@ -1,5 +1,7 @@
 module FriendlyTitleHelper
 
+  include FormatDate
+
   def self.included(base)
     base.extend FriendlyId
     base.friendly_id :slug_candidates, use: :slugged
@@ -11,8 +13,14 @@ module FriendlyTitleHelper
   def slug_candidates
     [
       :title,
+      [:title, :url_identifier]
+      [:title, ->{ start_date_to_append }],
       [:title, ->{ rand 1000 }]
     ]
+  end
+
+  def start_date_to_append
+    "-#{month_day_full_year(start_date)}"
   end
 
 end
